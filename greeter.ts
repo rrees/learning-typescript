623ce51ec7ba319f30671adde47a181da94abee1
Robert Rees <shudderfix@gmail.com>
2015-03-15 22:27:12 +0000

    Initial implementation of the tutorial

---
+++ greeter.ts
@@ -0,0 +1,8 @@
+
+function greeter(name: string) {
+    return "Hello " + name;
+}
+
+var user = "Robert";
+
+document.body.innerHTML = greeter(user);
---
+++ greeter2.ts
@@ -0,0 +1,22 @@
+module Greeter2 {
+
+    class TaxMan {
+        fullName: string;
+
+        constructor(public firstName, public lastName) {
+            this.fullName = firstName + " " + lastName;
+        }
+    }
+
+    interface Person {
+        firstName: string;
+        lastName: string;
+    }
+
+    function greeter(person: Person) {
+        return "Hello " + person.firstName + " " + person.lastName;
+    }
+
+    document.body.innerHTML = greeter({firstName: "Robert", lastName: "Rees"});
+    document.body.innerHTML = greeter(new TaxMan("Jon", "Davies"));
+}
